a74298c7a70f6189278b09567000c17d38409c73
Guilherme Camargo <1676894@TCS.com>
2019-04-11 11:12:52 -0300

    Estrutura back-end

---
+++ src/routes.js
@@ -0,0 +1,18 @@
+const express = require('express'); 
+const routes = express.Router();
+const multer = require('multer');
+const multerConfig = require('./config/multer');
+
+const BoxController = require('./controllers/BoxController');
+const FileController = require('./controllers/FileController');
+
+// Routes para Boxes
+routes.post('/boxes', BoxController.store);
+routes.get('/boxes/:id', BoxController.show);
+
+// Routes para Files
+routes.post('/boxes/:id/files', 
+  multer(multerConfig).single('file'), 
+  FileController.store);
+
+module.exports  = routes;
